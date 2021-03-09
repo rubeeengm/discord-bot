6323699a4e2b54a17cd34fc0946998166e5bed46
rubeeengm <rubeeengm@outlook.com>
2021-03-08 18:50:11 -0600

    + the bot return a random inspirational quote from API when someone types the message "$inspire" into the chat

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
 const client = new Discord.Client();
+
+function getQuote() {
+    return fetch('https://zenquotes.io/api/random')
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    return data[0]['q'] + ' - ' + data[0]['a']
+                })
+    ;
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 });
 
 client.on('message', message => {
-    if (message.content === "ping") {
-        message.reply("pong");
+    if  (message.author.bot) {
+        return;
+    }
+
+    if  (message.content === '$inspire') {
+        getQuote().then(
+            quote => message.channel.send(quote)
+        )
     }
 });
 
