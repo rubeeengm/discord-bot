6fea04f05b7586d69c4089fd67e4f4d0e6389c64
rubeeengm <rubeeengm@outlook.com>
2021-03-08 20:26:53 -0600

    + The user is able to add/remove encouraging messages for the bot to use when it detects a sad word. + New ability for get a list of user-submitted messages right from Discord + New ability to turn off and on whether the bot responds to sad words

--- main.js
+++ main.js
@@ -2,15 +2,32 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
+const ReplitDatabase = require('@replit/database')
+
 const client = new Discord.Client();
+const database = new ReplitDatabase();
 
 sadWords = ["sad", "depressed", "unhappy", "angry", "miserable"];
 
-encouragements = [
+starterEncouragements = [
     "Cheer up!"
     , "Hang in there."
     , "You are a great person / bot!"
 ]
+
+database.get('encouragements').then(encouragements => {
+    console.log(encouragements);
+
+    if (!encouragements || encouragements.length < 1) {
+        database.set('encouragements', starterEncouragements);
+    }
+});
+
+database.get('respondig').then(value => {
+    if (value == null) {
+        database.set('responding', true);
+    }
+});
 
 function getQuote() {
     return fetch('https://zenquotes.io/api/random')
@@ -21,6 +38,24 @@ function getQuote() {
                     return data[0]['q'] + ' - ' + data[0]['a']
                 })
     ;
+}
+
+function updateEncouragements(encouragingMessage) {
+    database.get('encouragements').then(encouragements => {
+        encouragements.push(encouragingMessage);
+
+        database.set('encouragements', encouragements);
+    });
+}
+
+function deleteEncouragment(index) {
+    database.get('encouragements').then(encouragements => {
+        if (encouragements.length > index) {
+            encouragements.splice(index, 1);
+
+            database.set('encouragements', encouragements);
+        }
+    });
 }
 
 client.on('ready', () => {
@@ -38,10 +73,54 @@ client.on('message', message => {
         )
     }
 
-    if  (sadWords.some(word => message.content.includes(word))) {
-        const encouragement = encouragements[Math.floor(Math.random() * encouragements.length)];
+    database.get('responding').then(responding => {
+        if (responding && sadWords.some(word => message.content.includes(word))) {
+            database.get('encouragements').then(encouragements => {
+                const encouragement = encouragements[
+                    Math.floor(
+                        Math.random() * encouragements.length
+                    )
+                ];
 
-        message.reply(encouragement);
+                message.reply(encouragement);
+            });
+        }
+    });
+
+    if (message.content.startsWith('$new')) {
+        encouragingMessage = message.content.split('$new ')[1];
+
+        updateEncouragements(encouragingMessage);
+
+        message.channel.send('New encouraging message added.');
+    }
+
+    if (message.content.startsWith('$del')) {
+        index = parseInt(message.content.split('$del ')[1]);
+
+        deleteEncouragment(index);
+
+        message.channel.send('Encouraging message deleted');
+    }
+
+    if (message.content.startsWith('$list')) {
+        database.get('encouragements').then(encouragements => {
+            message.channel.send(encouragements);
+        });
+    }
+
+    if (message.content.startsWith('$responding')) {
+        value = message.content.split('$responding ')[1];
+
+        if (value.toLowerCase() == 'true') {
+            database.set('responding', true);
+
+            message.channel.send('Responding is on.');
+            return;
+        }
+
+        database.set('responding', false);
+        message.channel.send('Responding is off.')
     }
 });
 
